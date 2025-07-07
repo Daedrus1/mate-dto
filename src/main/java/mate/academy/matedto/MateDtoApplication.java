e8280c35170ea07470c977b664af5773f7013c76
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-07-07 19:24:33 +0200

    main

---
+++ src/main/java/mate/academy/matedto/MateDtoApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.matedto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateDtoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MateDtoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/matedto/MateDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.matedto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateDtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
