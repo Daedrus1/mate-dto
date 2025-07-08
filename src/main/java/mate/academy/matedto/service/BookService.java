cc598376f809311b1ab414b3f8185ca6fa1886a4
Pavlo Mykhailyk <pmwonderchrono@gmail.com>
2025-07-08 12:21:22 +0200

    Commit 6 add Book Service and Implementation

--- src/main/java/mate/academy/matedto/service/BookService.java
+++ src/main/java/mate/academy/matedto/service/BookService.java
@@ -1,5 +1,14 @@
 package mate.academy.matedto.service;
 
-public interface BookService {
+import mate.academy.matedto.dto.BookDto;
+import mate.academy.matedto.dto.CreateBookRequestDto;
 
+import java.util.List;
+
+public interface BookService {
+    List<BookDto> getAll();
+
+    BookDto getById(Long id);
+
+    BookDto create(CreateBookRequestDto dto);
 }
