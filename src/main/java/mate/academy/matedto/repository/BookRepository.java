6cea98f328e9d657951dbd1c4651db2081cdf2f0
Pavlo Mykhailyk <163852106+Daedrus1@users.noreply.github.com>
2025-07-10 17:52:11 +0200

    Revert "Pr 1"

--- src/main/java/mate/academy/matedto/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.matedto.repository;
-
-import mate.academy.matedto.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
